743714ee46936dea5d5f11274a4016fc4024d181
aoi1 <tkhsaoi88@gmail.com>
2024-06-23 21:38:58 +0900

    Add subtle fix

---
+++ broken-deployment/step2/background.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cat <<EOF > my-deployment.yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: my-deployment
+  labels:
+    app: my-app
+spec:
+  replicas: 3
+  selector:
+    matchLabels:
+      app: my-app
+  template:
+    metadata:
+      labels:
+        app: my-app
+    spec:
+      containers:
+      - name: my-container
+        image: nginx:1.100
+        ports:
+        - containerPort: 80
+EOF
+
+kubectl apply -f my-deployment.yaml
+
