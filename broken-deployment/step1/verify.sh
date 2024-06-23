0754e9c9967f7a389019cf15b6f578bc704a7b72
aoi1 <tkhsaoi88@gmail.com>
2024-06-23 18:40:07 +0900

    Comment out for debug

--- broken-deployment/step1/verify.sh
+++ broken-deployment/step1/verify.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Get the names of all Pods created by the Deployment
 PODS=$(kubectl get pods -l app=my-app -o jsonpath='{.items[*].metadata.name}')
+echo $PODS
 
 # Check if each Pod is in Running state
-for POD in $PODS; do
-STATUS=$(kubectl get pod $POD -o jsonpath='{.status.phase}')
-if [ "$STATUS" != "Running" ]; then
-  exit 1
-fi
-done
-
-exit 0
+# for POD in $PODS; do
+# STATUS=$(kubectl get pod $POD -o jsonpath='{.status.phase}')
+# if [ "$STATUS" != "Running" ]; then
+#   exit 1
+# fi
+# done
+# 
+# exit 0
